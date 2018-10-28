7a49851720db13b851cc8eb1317efe7fb72b3b84
rifle-mark <rifle_rikki@163.com>
2018-10-28 11:00:45 +0800

    add some exercise

---
+++ exs/ex3.py
@@ -0,0 +1,37 @@
+# 打印，我现在数我的鸡
+print "I will now count my chickens:"
+
+# 计算25 加 30 除以 6， 并打印出来，它是Hens的数量
+print "Hens", 25 + 30 / 6
+# Roosters 的数量是： 100 减去 25 乘以 3 对 4取余
+print "Roosters", 100 - 25 * 3 % 4
+
+# 打印，现在我开始数蛋
+print "Now I will coun the eggs:"
+
+# 打印， 3 加 2 加 1 减 5 加 4 对 2 取余 减 1 除以 4 加 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# 打印， 是否3加2 小于 5减7？
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# 打印， 3 + 2 < 5 -7 这个表达式的值，这是个bool值
+print 3 + 2 < 5 - 7
+
+# 打印， 3 + 2 是多少？ 5
+print "What is 3 + 2?", 3 + 2
+# 打印， 5 - 7 是多少？ -2
+print "What is 5 - 7?", 5 - 7
+
+# 打印， 哦，这就是为什么值是False
+print "Oh that's why it's False."
+
+# 打印， 再多来点儿怎么样？
+print "How about some more."
+
+# 打印， 是否大于？ 5 > -2 的值
+print "Is it greater?", 3 + 2 > 5 - 7
+# 打印， 是否大于等于？ 5 >= -2 的值
+print "Is it greater or equal?", 5 >= -2
+# 打印， 是否小于等于？ 5 <= -2 的值
+print "Is it less or equal?", 5 <= -2
